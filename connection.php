02d5b067d9a7c8fceaec8e16a9f82b16e9c8b46d
Andy Maulana Yusuf <goingtoprofandy@gmail.com>
2019-05-29 14:54:51 +0700

    Update connection

--- connection.php
+++ connection.php
@@ -1,10 +1,15 @@
 <?php
-    $connectionInfo = array("UID" => "aitek230@aitek230", "pwd" => "Programer123##@@l", "Database" => "aitek230", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:aitek230.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    $name = "aitek230";
+    $key1 = "K9ReKKgFozmzy3nlt5HJnjQygmaURpkw2tj8MxTJSpdlxF1/BNwN1/aosI7qTwNNiI8qA4GJGfw6uPtbNpxbXw==";
+    $key2 = "a2d8nofNqlHWTGTbc9Zxwz/ocUN4WD1KnYiyWF8ANaQD2oJ0qtXDB+gnkBtY9ioRS1wfiyAHL0BhMyAGeXBR3A==";
+    $conn = "DefaultEndpointsProtocol=https;AccountName=".$name.";AccountKey=".$key1.";EndpointSuffix=core.windows.net";
 
-    if( !$conn ) {
-        echo "Connection could not be established.<br />";
-        die( print_r( sqlsrv_errors(), true));
-   }
+    $blobClient = BlobRestProxy::createBlobService($conn);
+    $createContainerOptions = new CreateContainerOptions();
+
+    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+    $createContainerOptions->addMetaData("key1", $key1);
+    $createContainerOptions->addMetaData("key2", $key2);
+
+    $containerName = "blockblobs".generateRandomString();
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
     require_once ("vendor/autoload.php");
     require_once ("connection.php");
-
-    if(isset($_POST['submit'])) {
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-
-        $query = sqlsrv_query($conn, "INSERT INTO todo (id, title, description) VALUES(DEFAULT, '$title', '$description')");
-        if($query === false) {
-            die( print_r( sqlsrv_errors(), true));
-        }
-        else {
-            echo ("<script>alert('Berhasil menambahkan aktivitas'); location.assign('https://aitek230.azurewebsites.net/');</script>");
-        }
-    }
 ?>
 
 <!DOCTYPE html>
@@ -31,53 +18,20 @@
     </style>
 </head>
 <body>
-    <h1>TODO APPS</h1>
-    <form method="post">
+    <h1>VISION APPS</h1>
+    <form method="post" enctype="multipart/form-data">
         <div>
-            <input type="text" name="title" placeholder="Input Title">
-        </div>
-        <div>
-            <textarea name="description" cols="30" rows="10"></textarea>
+            <input type="file" name="imageFile">
         </div>
         <div>
             <input type="submit" value="submit" name="submit">
         </div>
     </form>
     <br>
-    <table cellpadding="5" border="1px">
-        <thead>
-            <tr>
-                <th>NO</th>
-                <th>TITLE</th>
-                <th>DESCRIPTION</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-            $query = sqlsrv_query($conn, "SELECT * FROM todo", array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-            $count = sqlsrv_num_rows($query);
-
-            if($count === false || $count === 0){
-                echo ("
-                    <tr>
-                        <td colspan='3'>No Row Found</td>
-                    </tr>
-                ");
-            }
-            else {
-                $no = 0;
-                while ($row = sqlsrv_fetch_array($query)) {
-        ?>
-            <tr>
-                <td><?php echo ++$no; ?></td>
-                <td><?php echo $row['title']; ?></td>
-                <td><?php echo $row['description']; ?></td>
-            </tr>
-        <?php
-                }
-            }
-        ?>
-        </tbody>
-    </table>
+    <?php
+        if(isset($_POST['submit'])) {
+            
+        }
+    ?>
 </body>
 </html>
